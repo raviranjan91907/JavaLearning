87be4f203f2acb52403d66c179000170142ef522
raviranjan91907 <raviranjan91907@gmail.com>
2026-09-02 14:57:43 +0530

    if-else learning

--- src/ConditionalStatement/IfElseStatement/IfElseLearning.java
+++ src/ConditionalStatement/IfElseStatement/IfElseLearning.java
@@ -2,6 +2,15 @@ package ConditionalStatement.IfElseStatement;
 
 public class IfElseLearning {
     public static void main(String[] args){
-        
+        /*
+            if-else statement is used to make decision in a program
+         */
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote");
+        } else {
+            System.out.println("You are not eligible to vote");
+        }
     }
 }
