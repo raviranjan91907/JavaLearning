731318cdc91d103d14e236d0e41a9090e0ff66b7
raviranjan91907 <raviranjan91907@gmail.com>
2026-09-12 17:57:43 +0530

    Object Class Learning

--- src/ObjectOrientedProgramming/ObjectClass/ObjectClass.java
+++ src/ObjectOrientedProgramming/ObjectClass/ObjectClass.java
@@ -5,11 +5,42 @@ package ObjectOrientedProgramming.ObjectClass;
     that all java object can use
 */
 
+import java.util.Objects;
+
 class Laptop{
     String model;
     int price;
 
+    public String toString(){
+        return model+" "+price;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Laptop laptop = (Laptop) o;
+        return price == laptop.price && Objects.equals(model, laptop.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, price);
+    }
 }
+/*
+the above class dafination is same as the class below
+class Laptop extends object{}
+*/
 public class ObjectClass {
+    public static void main(){
+        Laptop obj1=new Laptop();
+        obj1.model="Lanovo";
+        obj1.price=100000;
+
+        Laptop obj2=new Laptop();
+        obj2.model="Lanovo";
+        obj2.price=100000;
+
+        System.out.println(obj1.equals(obj2));
+    }
 }
