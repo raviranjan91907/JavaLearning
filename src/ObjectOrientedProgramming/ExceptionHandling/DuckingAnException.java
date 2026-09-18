b66a4a9924cf4467b2e5b0164b3bea752fb90b1a
raviranjan91907 <raviranjan91907@gmail.com>
2026-09-18 23:49:43 +0530

    Ducking Exception Learning

--- src/ObjectOrientedProgramming/ExceptionHandling/DuckingAnException.java
+++ src/ObjectOrientedProgramming/ExceptionHandling/DuckingAnException.java
@@ -1,4 +1,28 @@
 package ObjectOrientedProgramming.ExceptionHandling;
 
+
+/*j
+    Exception Ducking means that instead of handling a checked exception inside the current method, we pass the responsibility of handling it to the calling method using the throws keyword
+ */
+
+import static java.lang.Class.forName;
+
+class A{
+    public void show() throws ClassNotFoundException{
+        Class.forName("Calc");
+    }
+}
 public class DuckingAnException {
+    static{
+        System.out.println("Class Loader");
+    }
+    public static void main(String args[]) {
+        A obj=new A();
+        try{
+            obj.show();
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("No Class Found");
+        }
+    }
 }
