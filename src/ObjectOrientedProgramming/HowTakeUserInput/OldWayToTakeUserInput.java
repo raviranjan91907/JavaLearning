64d01fc1cde2ecfced88cf7fb532068f752208da
raviranjan91907 <raviranjan91907@gmail.com>
2026-09-20 23:48:14 +0530

    Old way of taking User Input Learning

--- src/ObjectOrientedProgramming/HowTakeUserInput/OldWayToTakeUserInput.java
+++ src/ObjectOrientedProgramming/HowTakeUserInput/OldWayToTakeUserInput.java
@@ -1,4 +1,16 @@
 package ObjectOrientedProgramming.HowTakeUserInput;
 
-public class OldWayToTakeUserInput {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OldWayToTakeUserInput{
+    public static void main(String args[]) throws IOException {
+        InputStreamReader in =new InputStreamReader(System.in);
+        BufferedReader bf=new BufferedReader(in);
+
+        int num=Integer.parseInt(bf.readLine());
+
+        System.out.println(num);
+    }
 }
